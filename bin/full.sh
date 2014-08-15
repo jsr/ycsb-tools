ad1c888a4280bac5ce84d5c95e15876b1c9a59db
Jared Rosoff <forjared@gmail.com>
2014-08-15 16:10:55 -0400

    Load won't work distributed. just run it from one node

--- bin/full.sh
+++ bin/full.sh
@@ -3,17 +3,6 @@ source ./lib/validate-config
 outputdir=./output/$(date +%Y-%h-%d-%H:%M)
 mkdir -p $outputdir
 
-echo "output from this job will be stored in $outputdir"
-
-workload_name=${dataset}-${threads}threads-load
-./downloads/mongodb-linux-x86_64-2.6.0/bin/mongostat --noheaders --host $url --discover > $outputdir/${workload_name}-mongostat & 
-PID=$!
-sleep 3
-
-echo "Starting benchmark load at $(date +%Y-%h-%d-%H:%M)" 
-./YCSB/bin/ycsb load mongodb -P ./datasets/$dataset -p mongodb.url=$url -threads $threads -s &> $outputdir/${dataset}-${threads}threads-load
-kill $PID
-
 echo "Starting run phase at $(date +%Y-%h-%d-%H:%M)" 
 
 for threads in 8 16 32 64
